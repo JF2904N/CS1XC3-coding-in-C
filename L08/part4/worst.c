7032a873008e5106ec114a4d7ba7d74971f3c148
fongj13 <fongj13@pascal.cas.mcmaster.ca>
2023-03-24 11:21:45 -0400

    part2

---
+++ L08/part4/worst.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+int main (){
+   int x[3] = {1,2,3};
+   int out = x[5];
+   int q = 5/0;
+   bool pp = true;
+   printf(pp++);
+   if(x==3){
+    return 0;
+   }
+   else if(x==3){
+    return 0;
+   }
+}
